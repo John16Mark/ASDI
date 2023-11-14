787b14dc047d7457328db2a93cbdb0418ba2fc9e
John16Mark <jlma-113@hotmail.com>
2023-11-13 21:42:30 -0600

    Comentarios

--- ASDI.java
+++ ASDI.java
@@ -6,12 +6,10 @@ import java.util.Stack;
 public class ASDI implements Parser{
 
     private int i = 0;
-    private boolean hayErrores = false;
-    private Token preanalisis;
     private final List<Token> tokens;
-
     private final Stack<Object> pila = new Stack<>();
 
+    // Producciones
     private ArrayList<Object> Q = new ArrayList<>(Arrays.asList(TipoToken.SELECT, NoTerminal.D, TipoToken.FROM, NoTerminal.T));
     private ArrayList<Object> D_1 = new ArrayList<>(Arrays.asList(TipoToken.DISTINCT, NoTerminal.P));
     private ArrayList<Object> D_2 = new ArrayList<>(Arrays.asList(NoTerminal.P));
@@ -32,14 +30,15 @@ public class ASDI implements Parser{
     private ArrayList<Object> T3_1 = new ArrayList<>(Arrays.asList(TipoToken.IDENTIFICADOR));
     private ArrayList<Object> T3_2 = new ArrayList<>();
 
+    // Tabla, filas son no terminales, columnas son tokens
     private final ArrayList<ArrayList<Object>> tabla = new ArrayList<>();
     ArrayList<NoTerminal> filas = new ArrayList<>(Arrays.asList(NoTerminal.Q,NoTerminal.D,NoTerminal.P,NoTerminal.A,NoTerminal.A1,NoTerminal.A2,NoTerminal.A3,NoTerminal.T,NoTerminal.T1,NoTerminal.T2,NoTerminal.T3));
     ArrayList<TipoToken> columnas = new ArrayList<>(Arrays.asList(TipoToken.SELECT,TipoToken.FROM,TipoToken.DISTINCT,TipoToken.ASTERISCO,TipoToken.PUNTO,TipoToken.COMA,TipoToken.IDENTIFICADOR, TipoToken.EOF));
 
     public ASDI(List<Token> tokens){
         this.tokens = tokens;
-        preanalisis = this.tokens.get(i);
 
+        // Creación de la tabla
         for(int k=1; k<=11; k++){
             tabla.add(new ArrayList<Object>(Arrays.asList(null,null,null,null,null,null,null,null)));
         }
@@ -73,19 +72,7 @@ public class ASDI implements Parser{
         tabla.get(10).set(5, T3_2);
         tabla.get(10).set(6, T3_1);
         tabla.get(10).set(7, T3_2);
-        /*
-        for(int k=0; k<11; k++){
-            for(int j=0; j<8; j++){
-                if(tabla.get(k).get(j) !=null) {
-                    System.out.print("\033[95m");
-                }
-                System.out.print(tabla.get(k).get(j));
-                System.out.print("\033[0m");
-                System.out.print(" ");
-            }
-            System.out.print("\n");
-        }
-        */
+
     }
 
     @Override
@@ -93,10 +80,11 @@ public class ASDI implements Parser{
         pila.push(TipoToken.EOF);
         pila.push(NoTerminal.Q);
 
+        // Analizando todos los tokens
         while(i<tokens.size()){
-            //System.out.println("Comparando: "+pila.peek()+" - "+tokens.get(i).tipo);
+            // Si el tope de la pila y el token apuntado son iguales
             if(pila.peek() instanceof TipoToken && pila.peek() == tokens.get(i).tipo){
-                //System.out.println("Iguales: "+pila.peek());
+                // Si el tope de la pila y el token apuntado son fin de cadena -> Terminar el análisis
                 if(pila.peek() == TipoToken.EOF && tokens.get(i).tipo == TipoToken.EOF){
                     System.out.println("\033[94mAnálisis Sintáctico correcto\033[0m");
                     return true;
@@ -104,21 +92,24 @@ public class ASDI implements Parser{
                 i++;
                 pila.pop();
             }
+            // Si el tope de la pila es un NoTerminal
             else if(pila.peek() instanceof NoTerminal){
                 int fila = filas.indexOf(pila.peek());
                 int columna = columnas.indexOf(tokens.get(i).tipo);
 
+                // Si no hay nada en la celda -> error
                 if(tabla.get(fila).get(columna) == null){
                     System.out.println("\033[91mSe encontraron errores\033[0m");
                     return false;
                 } else {
+                    // Sacar un elemento de la pila
                     pila.pop();
                     ArrayList<Object> celda = (ArrayList<Object>) tabla.get(fila).get(columna);
+                    
+                    // Añadir todos los elementos de la producción en orden inverso
                     for(int j = celda.size()-1; j>=0 ; j--){
-                        //System.out.print(celda.get(j));
                         pila.push(celda.get(j));
                     }
-                    //System.out.print("\n");
                 }
             } else {
                 System.out.println("\033[91mSe encontraron errores\033[0m");
